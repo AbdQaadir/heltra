6a6c238ea6ab89f13db5a36d9d111f7474a6e686
Lateef Quadri <lateef9816@gmail.com>
2024-06-10 14:23:04 +0100

    added homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,159 @@
-import Image from "next/image";
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  ArrowRight,
+  ChartLineUp,
+  Folder,
+  Hamburger,
+  HandHeart,
+  Heart,
+  List,
+  Target,
+  Toolbox,
+} from "@phosphor-icons/react";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
+    <main>
+      {/* Header */}
+      <div className="flex items-center justify-between w-full h-[80px] py-4 px-3 md:px-6">
+        <h1 className="text-2xl font-bold">Heltra</h1>
+
+        <nav className="hidden md:flex items-center space-x-4">
+          <a href="#" className="text-gray-700">
+            Home
           </a>
+          <a href="#" className="text-gray-700">
+            Features
+          </a>
+          <a href="#" className="text-gray-700">
+            Pricing
+          </a>
+          <a href="#" className="text-gray-700">
+            Contact
+          </a>
+        </nav>
+
+        <div className="flex gap-2 items-center">
+          <Button
+            variant="secondary"
+            className="flex gap-1 items-center text-small px-4 py-2 rounded-lg"
+          >
+            Login
+            <ArrowRight size={20} />
+          </Button>
+
+          <Button variant="ghost" className="md:hidden">
+            <List size={30} />
+          </Button>
         </div>
       </div>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      {/* Hero section */}
+      <div className="bg-gradient-to-br from-[#a49c9c] to-[#d8d0d0]">
+        <div className="min-h-[60vh] md:min-h-[90vh] w-[100%] max-w-[700px] mx-auto flex flex-col md:items-center justify-center px-4 md:text-center gap-4 md:gap-5">
+          <h1 className="text-3xl font-bold md:text-4xl lg:text-5xl md:leading-loose">
+            Track Your Health Effortlessly with Heltra
+          </h1>
+          <p
+            className="
+          text-md text-gray-700 mt-1 leading-1
+        "
+          >
+            Monitor your vitals, access medical records, and stay on top of your
+            health with our comprehensive tracker.
+          </p>
+          <Button variant="secondary" size="lg">
+            Sign Up Now
+          </Button>
+        </div>
       </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+      {/* Features */}
+      <div>
+        <h2 className="text-2xl font-bold text-center mt-10">Features</h2>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        <div className="flex flex-col md:flex-row items-center justify-center gap-5">
+          {[
+            {
+              icon: <Heart size={50} />,
+              title: "Vitals Tracking",
+              description:
+                "Log and monitor key health metrics like blood pressure, heart rate, and weight. View your data in easy-to-read charts and set reminders for regular checks.",
+            },
+            {
+              icon: <Folder size={50} />,
+              title: "Medical Records",
+              description:
+                "Securely store and manage your medical documents in one place. Access your prescriptions, lab results, and doctor's notes from any device.",
+            },
+            {
+              icon: <Toolbox size={50} />,
+              title: "Activity Monitoring",
+              description:
+                "Log daily activities including workouts and steps. Sync with fitness devices to track your progress and achieve your fitness goals.",
+            },
+          ].map((feature) => (
+            <div
+              key={feature.title}
+              className="flex flex-col items-center justify-center gap-4 bg-white p-4 rounded-lg shadow-md min-h-[400px] max-w-[90%] w-[330px] md:w-[350px]"
+            >
+              {feature.icon}
+              <h3 className="text-lg font-bold text-center">{feature.title}</h3>
+              <p className="text-gray-700 text-center mt-1">
+                {feature.description}
+              </p>
+            </div>
+          ))}
+        </div>
       </div>
+
+      {/* Benefits section */}
+      <div>
+        <h2 className="text-2xl font-bold text-center mt-10">Benefits</h2>
+
+        <div className="flex flex-col md:flex-row items-center justify-center gap-5">
+          {[
+            {
+              icon: <ChartLineUp size={50} />,
+              title: "Improved Health",
+              description:
+                "Stay on top of your health with regular monitoring and personalized insights.",
+            },
+            {
+              icon: <HandHeart size={50} />,
+              title: "Efficient Care",
+              description:
+                "Access your medical records anytime, anywhere for quick consultations and appointments.",
+            },
+            {
+              icon: <Target size={50} />,
+              title: "Fitness Goals",
+              description:
+                "Track your fitness progress and set achievable goals with our activity monitoring feature.",
+            },
+          ].map((benefit) => (
+            <div
+              key={benefit.title}
+              className="flex flex-col items-center justify-center gap-4 bg-white p-4 rounded-lg shadow-md min-h-[400px] max-w-[90%] w-[330px] md:w-[350px]"
+            >
+              {benefit.icon}
+              <h3 className="text-lg font-bold text-center">{benefit.title}</h3>
+              <p className="text-gray-700 text-center mt-1">
+                {benefit.description}
+              </p>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      {/* Footer */}
+      <footer className="flex flex-col content-center h-[200px] border-t-1 border-t-gray-800 text-grey-900 py-24 mt-24 text-center">
+        <p className="text-sm">
+          &copy; {new Date().getFullYear()} Heltra. All rights reserved.
+        </p>
+      </footer>
     </main>
   );
 }
