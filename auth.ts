3d3f11823755b60163c00176252d12fb37d6a043
Lateef Quadri <lateef9816@gmail.com>
2024-09-12 11:15:11 +0100

    fix-remove-callback

--- auth.ts
+++ auth.ts
@@ -9,20 +9,4 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     url: process.env.SUPABASE_URL as string,
     secret: process.env.SUPABASE_SERVICE_ROLE_KEY as string,
   }),
-  callbacks: {
-    async session({ session, user }) {
-      const signingSecret = process.env.SUPABASE_JWT_SECRET;
-      if (signingSecret) {
-        const payload = {
-          aud: "authenticated",
-          exp: Math.floor(new Date(session.expires).getTime() / 1000),
-          sub: user.id,
-          email: user.email,
-          role: "authenticated",
-        };
-        (session as any).supabaseAccessToken = jwt.sign(payload, signingSecret);
-      }
-      return session;
-    },
-  },
 });
